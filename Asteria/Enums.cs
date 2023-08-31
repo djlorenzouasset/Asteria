1763f6ea040a40bc8f4f02b50f6aaf25b95460ba
ᴅᴊʟᴏʀ3xᴢᴏ <lorenzogame85@gmail.com>
2023-08-31 02:57:51 +0200

    enums v2.1.0

--- Asteria/Enums.cs
+++ Asteria/Enums.cs
@@ -1,4 +1,33 @@
-﻿namespace Asteria;
+﻿using System.ComponentModel;
+
+namespace Asteria;
+
+public enum EInstallType : byte
+{
+    [Description("Local Installation")]
+    LocalArchives = 0,
+
+    [Description("Fortnite Live")]
+    FortniteLive = 1
+}
+
+public enum Design : byte 
+{
+    [Description("Custom Background")]
+    CustomBackground = 0,
+
+    [Description("Rarity Background")]
+    RarityBackground = 1
+}
+
+public enum DiscordPresence : byte
+{
+    [Description("Always")]
+    Always = 0,
+
+    [Description("Disabled")]
+    Disabled = 1
+}
 
 public enum CosmeticTypes : byte
 {
